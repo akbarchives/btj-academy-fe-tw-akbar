60d21d4391437feb82732c7b7cfd717722ace51e
akbarchives <rizqiakbar2412@gmail.com>
2023-12-20 10:06:29 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2b3499",
+        secondary: "#FF6c22",
+        third: "#FFE0D0",
+      },
+    },
+  },
+  plugins: [],
+};
